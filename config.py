3cfe8d0535439ae712feea6cba08ccfcefae2bf6
Itsuki Kuwahara <248c117c@stu.kobe-u.ac.jp>
2025-11-01 14:35:06 +0900

    GenerationConfifgのrepetition_pnealtyもコメントアウト

--- config.py
+++ config.py
@@ -41,7 +41,7 @@ class GenerationConfig:
     do_sample: bool = True   # サンプリングを行うかどうか
     top_p: float = 0.8       # top-pサンプリング
     top_k: int = 20          # top-kサンプリング
-    repetition_penalty: float = 1.1  # 繰り返しペナルティ
+    # repetition_penalty: float = 1.1  # 繰り返しペナルティ
     
 @dataclass 
 class DataConfig:
